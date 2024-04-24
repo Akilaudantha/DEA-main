a156fac896847c737b34b14ada6415a4d6051a46
dasun2000 <167028859+dasun2000@users.noreply.github.com>
2024-04-24 12:03:48 +0530

    Re-Submit payment

---
+++ MainProject/src/java/newpackage/Payments.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author MSI KATANA
+ */
+@WebServlet(name = "Payments", urlPatterns = {"/Payments"})
+public class Payments extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Payments</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Payments at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String Name=request.getParameter("Name");
+        String Address=request.getParameter("cn");
+        int CardNumber=Integer.parseInt(request.getParameter("card"));
+       String confirmed = request.getParameter("confirmed");
+
+         if (confirmed != null && confirmed.equals("1")){
+              String ExpireDate = request.getParameter("date");
+    String CVCcode = request.getParameter("cvc");
+         
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Payments.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         String url="jdbc:mysql://localhost:3306/mainDEA";
+         Connection con;
+        try {
+            con = DriverManager.getConnection(url,"root","");
+          Statement st=con.createStatement();
+          String q1="INSERT INTO Payment VALUES('"+Name+"','"+Address+"','"+CardNumber+"','"+ExpireDate+"','"+CVCcode+"')";
+          st.executeUpdate(q1);
+        } catch (SQLException ex) {
+            Logger.getLogger(Payments.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        response.sendRedirect("Thankyou.html");
+         
+      //  processRequest(request, response);
+    }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
